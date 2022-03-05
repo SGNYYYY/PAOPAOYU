9432b6a274bdb2293bedef9d5ab1c9c31bf3305d
SGNYYYY <1162814055@qq.com>
2022-03-05 20:35:35 +0800

    init

--- miniprogram/config.js
+++ miniprogram/config.js
@@ -10,12 +10,9 @@ const config = {
   host,
 
   // 云开发环境 ID
-  envId: 'release-b86096',
+  envId: 'sgnyyyy-0128',
   // envId: 'test-f0b102',
 
-  // 云开发-存储 示例文件的文件 ID
-  demoImageFileId: 'cloud://release-b86096.7265-release-b86096-1258211818/demo.jpg',
-  demoVideoFileId: 'cloud://release-b86096.7265-release-b86096/demo.mp4',
 }
 
 module.exports = config
